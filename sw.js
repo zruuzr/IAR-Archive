40c18c1cec6a3ae1cc216c3d3f8a831ca6e75f98
Zenvex <280889794+zruuzr@users.noreply.github.com>
2026-09-24 08:29:51 -0400

    Update CACHE_VERSION to v12

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
      - Controller change triggers auto-reload (handled in index.html)
    ============================================================ */
 
-const CACHE_VERSION = 'v11';
+const CACHE_VERSION = 'v12';
 const CACHE_NAME = `iar-archive-${CACHE_VERSION}`;
 
 const APP_SHELL = [
